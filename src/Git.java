26bd720354a8cb8090f7be7e7612a9571f7b5c88
PawOgo <pawelogorzeja@outlook.com>
2019-11-12 19:41:37 +0100

    pierwsza zmiana w projekcie

--- src/Git.java
+++ src/Git.java
@@ -1,7 +1,11 @@
-public class Git {
-}
+package com.company;
+
+// sout -> System.out.prinln();
+// ctrl + D -> duplikowanie lini kodu
 public class Main {
 
-    public static void main(string[]args)
-
+    public static void  main (String[] args){
+        System.out.println("hello world!");
+        System.out.println("publikujemy pierwszy program!!!");
+    }
 }
