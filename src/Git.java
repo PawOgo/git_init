e385d8067b58a719eb62a0c76ccdf0847686a4a3
PawOgo <pawelogorzeja@outlook.com>
2019-11-12 19:57:05 +0100

    druga zmiana w projekcie

--- src/Git.java
+++ src/Git.java
@@ -7,5 +7,6 @@ public class Main {
     public static void  main (String[] args){
         System.out.println("hello world!");
         System.out.println("publikujemy pierwszy program!!!");
+        System.out.println("kolejna zmiana");
     }
 }
